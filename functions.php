57a514324ee1addd87d4ed87b58d2a0561bd639f
ttt1091 <tez1091@yahoo.co.jp>
2021-04-01 15:37:37 +0900

    a

---
+++ functions.php
@@ -0,0 +1,11 @@
+<?php
+
+function add_styles() {
+  wp_enqueue_style( 'default_styles', get_template_directory_uri().'/assets/css/styles.css' );
+}
+add_action( 'wp_enqueue_scripts', 'add_styles' );
+
+function output_title() {
+  add_theme_support('title-tag');
+}
+add_action('after_setup_theme', 'output_title');
---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.2/css/bulma.min.css">
+  <?php wp_head(); ?>
+</head>
+<body>
+<section class="hero">
+  <div class="hero-body">
+    <div class="container">
+      <h1 class="title has-text-light">
+        <?php bloginfo( 'name' ); ?>
+      </h1>
+      <h2 class="subtitle is-6 has-text-light">
+        subtitle
+      </h2>
+    </div>
+  </div>
+  <div class="dust"></div>
+</section>
+
+<section class="section">
+  <div class="container">
+    コンテンツa
+  <div>
+</section>
+
+</body>
+</html>
